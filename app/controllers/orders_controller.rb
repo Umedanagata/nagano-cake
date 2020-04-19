35ffb462d3ee9535506e3681e3d58b848a5e1864
hiroki-ume <h.ume1219bknb@gmail.com>
2020-04-19 22:05:18 +0900

    お届け先選択フォームの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,6 +12,15 @@ class OrdersController < ApplicationController
     @order = current_customer.orders.new(order_params)
     @cart_item = current_customer.cart_items
     @cart_items = @cart_item.all
+    if params[:address_select] == "0"
+      @order.postcode = current_customer.postcode
+      @order.address = current_customer.address
+      @order.ship_name = current_customer.family_name + current_customer.last_name
+    elsif params[:address_select] == "2"
+      @order.postcode = params[:postcode]
+      @order.address = params[:address]
+      @order.ship_name = params[:ship_name]
+    end
   end
 
   def create
@@ -27,4 +36,11 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:payment_method, :postcode, :address, :ship_name)
   end
+  # def address_select(order_params)
+  #   if address_select == 0
+  #     @order.address == current_customer.addresses
+  #   elsif address_select == 2
+  #     @order.address == current_customer.addresses.new(order_params)
+  #   end
+  # end
 end

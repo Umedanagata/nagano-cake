4ea7bc0c9c06ad6636f4a00a1a609b62ff700a71
chihirotanaka <senno330@gmail.com>
2020-04-22 08:11:03 +0000

    [注文ビューページ機能追加]

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,17 +1,17 @@
 class Admins::OrdersController < ApplicationController
+   Time.zone = 'Tokyo'
+
   def index
-  	@orders =Order.page(params[:page]).per(10)
-  	@order_items =OrderItem.all
-    @customers = Customer.all
+  	@orders =Order.page(params[:page]).per(3)
   end
 
   def show
-  	@order =Order.all
-  	@order_items =OrderItem.all
+  	@order = Order.all
   end
-
+  # @customer = Customer.find(@order.customer_id)
   def update
-  	@order =Order.all
-  	@order_items =OrderItem.all
+    @order = Order.find(customer.id)
+  # 注文id＝＝注文商品の持つ商品id
+    @order.id = @order.order_items.id
   end
 end

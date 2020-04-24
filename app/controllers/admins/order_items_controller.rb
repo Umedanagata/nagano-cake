55cc494ae5cd41e43bdc61e241bcca56005864ae
chihirotanaka <senno330@gmail.com>
2020-04-24 04:11:51 +0000

    [test]

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -1,6 +1,25 @@
 class Admins::OrderItemsController < ApplicationController
   def update
-  	@orders =Order.all
-  	@order_items =OrderItem.all
+  	@orderitem = OrderItem.find(params[:id])
+    @orderitem.update(orders_params)
+    if @order == :deposit
+     @product_status = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+      @order_items = @order.order_items.new
+      @order_items.item_id = cart_item.item.id
+      @order_items.name = cart_item.item.name
+      @order_items.price = cart_item.item.price
+      @order_items.quantity = cart_item.quantity
+      product_status.update
+    end
+    elsif
+
+    else
+      redirect_to :show
+    end
+  end
+
+  def production_params
+    params.require(:order_item).permit(:production_status)
   end
 end
--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -10,9 +10,27 @@ class Admins::OrdersController < ApplicationController
   end
 
   def update
-    @order = Order.find(params.id)
-    @order.customer_id = customer_id
-  # 注文id＝＝注文商品の持つ商品id
-    @order.id = @order.order_items.id
+      # 注文id＝＝注文商品の持つ商品id
+    @order = Order.find(params[:id])
+    @order.update(orders_params)
+    if @order == :deposit
+     @product_status = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+      @order_items = @order.order_items.new
+      @order_items.item_id = cart_item.item.id
+      @order_items.name = cart_item.item.name
+      @order_items.price = cart_item.item.price
+      @order_items.quantity = cart_item.quantity
+      product_status.update
+    end
+    else
+      redirect_to :show
+    end
+
+  private
+
+  def orders_params
+    params.require(:order).permit(:orders_status)
   end
+
 end

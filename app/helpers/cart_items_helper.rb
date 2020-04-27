38ddde805c9953845832961e2532c78977558f75
hiroki-ume <h.ume1219bknb@gmail.com>
2020-04-27 11:56:52 +0900

    helper不具合修正

--- app/helpers/cart_items_helper.rb
+++ app/helpers/cart_items_helper.rb
@@ -1,5 +1,5 @@
 module CartItemsHelper
   def cart_item_price(cart_item)
-    tax_price = (cart_item.item.price * 1.1).to_s(:delimited).floor
+    tax_price = (cart_item.item.price * 1.1).floor.to_s(:delimited)
   end
 end

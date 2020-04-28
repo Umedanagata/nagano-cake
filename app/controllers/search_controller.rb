e4ece7c59f695dc4dde8833ff9ed5dccda2a4e4b
tanaka-yu3 <tanashan3@gmail.com>
2020-04-28 04:37:54 +0000

    検索昨日調整、商品新規登録機能調整

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,25 +9,11 @@ class SearchController < ApplicationController
   private
 	def search_for(model,content,method)
 		if model == 'customer'
-			if method == 'perfect'
-				Customer.where(family_name: content)
-			elsif method == 'forward'
-				Customer.where('family_name LIKE ?', content+'%')
-			elsif method == 'backward'
-				Customer.where('family_name LIKE ?', '%'+content)
-			else
-				Customer.where('family_name LIKE ?', '%'+content+'%')
-			end
-		elsif model == 'item'
-			if method == 'perfect'
-				Item.where(name: content)
-			elsif method == 'forward'
-				Item.where('name LIKE ?', content+'%')
-			elsif method == 'backward'
-				Item.where('name LIKE ?', '%'+content)
-			else
-				Item.where('name LIKE ?', '%'+content+'%')
-			end
+		   method == 'partial'
+		   Customer.where('family_name LIKE ?', '%'+content+'%')
+	    else
+		   method == 'partial'
+		   Item.where('name LIKE ?', '%'+content+'%')
 		end
 	end
 end

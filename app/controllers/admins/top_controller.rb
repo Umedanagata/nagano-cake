9547ea4940aec2abbee984cb3773cd7a8cdc99e4
tanaka-yu3 <tanashan3@gmail.com>
2020-04-29 01:06:10 +0000

    最終調整

--- app/controllers/admins/top_controller.rb
+++ app/controllers/admins/top_controller.rb
@@ -2,6 +2,6 @@ class Admins::TopController < ApplicationController
 	layout 'top'
 	before_action :authenticate_admin!
   def top
-  	@order = Order.where("created_at >= ?", Date.today)
+  	@order = Order.where("created_at == ?", Date.today)
   end
 end
